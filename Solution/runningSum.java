f802275191b8b671f626723be02f84a4d4e27fd8
DecimoDevs <marlonadiguemartin548@gmail.com>
2023-02-16 19:58:41 +0800

    update config : review

--- Solution/runningSum.java
+++ Solution/runningSum.java
@@ -1,15 +1,18 @@
 class solution {
     public int[] runningSum(int[] nums) {
         // declare an array of answers for the output
-        int[] answer = new int[nums.length];
+        int[] answers = new int[nums.length];
+
         // declare a starting point for the answers
-        answer[0] = nums[0];
+        answers[0] = nums[0];
 
         // loop trough the array of nums
-        for (int i = 1; i < nums.length; i++) {
-            // answer will be the sum of the current index to the next index
-            answer[i] = answer[i - 1] + nums[i];
+        int size = nums.length;
+        for (int i = 0; i < size; i++) {
+            answers[i] = nums[i - 1] + nums[i];
         }
-        return answer;
-    }
+        
+        // answer will be the sum of the current index to the next index
+        return answers;  
+      }
 }
