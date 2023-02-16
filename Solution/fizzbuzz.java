746c423e03da6d02e1cc596c1c9b132e6624bbfd
DecimoDevs <marlonadiguemartin548@gmail.com>
2023-02-16 19:52:03 +0800

    update config : review

--- Solution/fizzbuzz.java
+++ Solution/fizzbuzz.java
@@ -5,23 +5,24 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> list = new ArrayList<>();
 
-        // loop over or iterate over the given number.
-        for (int i = 1; i <= n; i++) {
-            // if the iteration index is divisible by 15 then it will print fizzbuzz
+        // linear loop to the list
+        for (int i = 1; i < n; i++) {
+            // check if the index is divisible by 15, 3 and 5
             if (i % 15 == 0) {
+                // then print
                 list.add("FizzBuzz");
-            // if the iteration index is divisible by 3 then it will print fizz
             } else if (i % 3 == 0) {
+                // then print
                 list.add("Fizz");
-            // if the iteration index is divisible by 5 then it will print buzz
             } else if (i % 5 == 0) {
+                // then print
                 list.add("Buzz");
-            } 
-            // hence print the iteration index
-            else {
+            } else {
+                // print all the value of index in the list while looping
                 list.add(String.valueOf(i));
             }
         }
         return list;
     }
+        
 }
